90f5656aefbe85a86ca261c532f2fa1a805254a9
Matt <matt.titmarsh@gmail.com>
2023-02-03 05:36:24 +1000

    fix user logout issue with jquery_ujs

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  devise_scope :user do
+    get 'sign_out' => 'devise/sessions#destroy'
+  end
 end
