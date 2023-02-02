d5b978bb78e1b93b9e9342fb61a6609481a5bec1
Matt <matt.titmarsh@gmail.com>
2023-02-03 06:39:31 +1000

    Add competitors table that contains an overview of the competition

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -24,4 +24,20 @@ class CompetitionsController < ApplicationController
       render 'new'
     end
   end
+
+  private
+
+  helper_method def earliest_and_latest_weight_record_by_user
+    @earliest_and_latest_weight_record_by_user || begin
+     @competition.weight_records.preload(:user).default_order.group_by(&:user).map do |user, weight_records|
+      [
+        user,
+        {
+          earliest: weight_records.last,
+          latest: weight_records.first
+        }
+      ]
+     end.to_h
+    end
+  end
 end
