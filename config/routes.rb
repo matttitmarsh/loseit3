46a049389267d2ce73784edbf45e7c5cb60b9afe
Matt <matt.titmarsh@gmail.com>
2023-02-03 06:27:28 +1000

    Stop overriding devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'home#index'
 
   devise_scope :user do
-      get 'login', to: 'devise/sessions#new'
-  end
-
-  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
     get 'signup', to: 'devise/registrations#new'
-  end
-
-  devise_scope :user do
     get 'sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
   resources :competitions do
     resources :weight_records
   end
