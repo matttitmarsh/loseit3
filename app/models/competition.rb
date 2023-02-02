56dd2eefaecf550f111237a2e31a479ea0a3cdcf
Matt <matt.titmarsh@gmail.com>
2023-02-03 06:18:42 +1000

    Add Competitor model

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,2 +1,4 @@
 class Competition < ApplicationRecord
+  has_many :competitors
+  has_many :users, through: :competitions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_30_233737) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_31_033443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_233737) do
     t.date "finish_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "competitors", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "competition_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["competition_id"], name: "index_competitors_on_competition_id"
+    t.index ["user_id"], name: "index_competitors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_30_233737) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "competitors", "competitions"
+  add_foreign_key "competitors", "users"
 end
