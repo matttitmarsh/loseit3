84fe65c720364fde2fe6fea90ea1e1700789354a
Matt <matt.titmarsh@gmail.com>
2023-02-03 04:42:40 +1000

    Add and configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'home#index'
+
+  devise_scope :user do
+      get 'login', to: 'devise/sessions#new'
+  end
 end
