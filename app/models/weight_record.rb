4c63538ca18412e6ab50f78d72d4a66afe8dd715
Matt <matt.titmarsh@gmail.com>
2023-02-03 06:33:13 +1000

    Sort weight records by effective_date on competition show page

--- app/models/weight_record.rb
+++ app/models/weight_record.rb
@@ -4,4 +4,7 @@ class WeightRecord < ApplicationRecord
 
   validates :weight, presence: true, numericality: true
   validates :effective_date, presence: true
+
+
+  scope :default_order, -> { order(effective_date: :desc) }
 end
