5a77c9885511a550dfcd426354cf0fc65f03e629
Matt <matt.titmarsh@gmail.com>
2023-02-03 06:40:36 +1000

    Fix incorrect users association on competition model

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,5 +1,5 @@
 class Competition < ApplicationRecord
   has_many :competitors
-  has_many :users, through: :competitions
   has_many :weight_records
+  has_many :users, through: :weight_records
 end
