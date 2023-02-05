8f85464cd2ff24afe0b6ef461f4c6b2fc789a452
Matt <matt.titmarsh@gmail.com>
2023-02-06 08:06:53 +1000

    Fix user signup redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -16,4 +16,9 @@ class RegistrationsController < Devise::RegistrationsController
                                   :password_confirmation,
                                   :current_password)
   end
+
+  private
+  def after_sign_up_path_for(resource)
+    competitions_path
+  end
 end
